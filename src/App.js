d23ac0bf59a2189e227dd8df2206bef8e29123c5
RakeshAhire <rocky5509ahire@gmail.com>
2022-11-10 18:52:59 +0530

    Navbar ready

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <Home />
-      hello
     </div>
   );
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div>
       <Navbar />
-      {/* <Routing /> */}
+      <Routing />
       <Footer />
     </div>
   )
